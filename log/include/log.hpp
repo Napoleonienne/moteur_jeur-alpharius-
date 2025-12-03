fa97dccd4c47d7bdeba52420646b6b0a28678242
guy matthias <eliassaint.matthias@gmail.com>
2025-12-03 17:53:11 +0100

    n

---
+++ log/include/log.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+class log{
+    log(std::string description);
+
+    void init();
+}
---
+++ log/src/log.cpp
@@ -0,0 +1,8 @@
+#include <log.hpp>
+#include <spdlog/spdlog.h>
+#include <spdlog/async.h>
+
+
+
+
+lo
