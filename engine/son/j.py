954ad87cb3abaf6891bf086f6c5902f7013e81bc
napoléon <eliassaint.matthias@gmail.com>
2026-02-20 13:45:48 +0100

    1

---
+++ engine/son/j.py
@@ -0,0 +1,13 @@
+def partition(n,m):
+    if n ==0:
+        return 0
+    
+    if n<0 abd
+    d = (n-2 else 0      ,
+         
+         )
+
+
+    j = partition(d[0])+1
+    i = partition(d[1])+1
+    return i+j
